75085d6f111a346a12cbcc4b6bd0769d7489499d
Collin Swisher <collin.swisher@trojans.dsu.edu>
2024-05-18 17:29:41 -0500

    Add service worker support

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,3 @@
 export default {
-    ignores: ["./src/pkg/*"]
+    ignores: ["src/pkg/*"]
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { NavBar } from './Components/navbar';
 import { Login } from './Pages/Login/Login'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import init from './pkg/wasm'
 
@@ -26,6 +27,11 @@ init("/app/wasm_bg.wasm").then(() => {
         </React.StrictMode>
       );
 })
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
